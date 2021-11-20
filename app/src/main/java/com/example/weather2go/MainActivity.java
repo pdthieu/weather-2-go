62a20caaa43632464f0ef102984e24ff4af93eb9
geschmackcr7 <trunghieuronaldo@gmail.com>
2021-11-20 17:50:41 +0700

    tabbar and 2 fragment

---
+++ app/src/main/java/com/example/weather2go/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.weather2go;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle();
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new FragmentMap(), "MAP");
+        vpAdapter.addFragment(new FragmentListLocation(), "LIST LOCATION");
+        viewPager.setAdapter(vpAdapter);
+
+    }
+}
