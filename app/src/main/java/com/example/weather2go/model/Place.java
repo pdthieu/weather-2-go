2e9f87c41ef37eca6add452737255ee0eda4aae3
haminh0307 <haminh0307@gmail.com>
2021-11-22 15:44:10 +0700

    List Place using RecyclerView

---
+++ app/src/main/java/com/example/weather2go/model/Place.java
@@ -0,0 +1,22 @@
+package com.example.weather2go.model;
+
+public class Place {
+    private double Lat;
+    private double Lon;
+
+    public double getLat() {
+        return Lat;
+    }
+
+    public void setLat(double lat) {
+        Lat = lat;
+    }
+
+    public double getLon() {
+        return Lon;
+    }
+
+    public void setLon(double lon) {
+        Lon = lon;
+    }
+}
